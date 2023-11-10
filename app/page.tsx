eb09828523ccf3d5658eaecaf12039a32c69b063
Havish Vadlamani <54392756+havish2001@users.noreply.github.com>
2023-11-10 07:27:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ import './globals.css';
 import Quote from './components/Quote';
 import SearchBar from './components/SearchBar';
 
-export default function Home({ initialQuote }: { initialQuote: any }){
+type HomeProps = {
+  initialQuote: any;
+};
+
+function Home({ initialQuote }: HomeProps) {
   const [quote, setQuote] = useState(initialQuote);
 
   const fetchRandomQuote = async () => {
@@ -17,8 +21,7 @@ export default function Home({ initialQuote }: { initialQuote: any }){
       }
 
       const data = await response.json();
-      console.log('Random Quote API Response:', data); // Log the API response data
-
+      console.log('Random Quote API Response:', data);
       setQuote(data);
     } catch (error) {
       console.error('Error fetching random quote:', error);
@@ -34,13 +37,10 @@ export default function Home({ initialQuote }: { initialQuote: any }){
       }
 
       const data = await response.json();
-      console.log('Search API Response:', data); // Log the API response data
+      console.log('Search API Response:', data);
       const randomIndex = Math.floor(Math.random() * data.length);
       const randomQuote = data[randomIndex];
-      // setQuote(randomQuote);
-      // Handle the response data as needed
-      // For example, update quote state based on the search results
-      setQuote(data.results[0]); // Assuming you want to display the first search result
+      setQuote(data.results[0]);
     } catch (error) {
       console.error('Error searching quotes:', error);
     }
@@ -62,3 +62,5 @@ export default function Home({ initialQuote }: { initialQuote: any }){
     </div>
   );
 }
+
+export default Home;
